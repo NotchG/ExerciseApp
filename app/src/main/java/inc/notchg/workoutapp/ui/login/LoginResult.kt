35175136772742be8515d3822e2bf20b2dab0aa7
NotchG <vidoghibran7@gmail.com>
2020-06-15 18:59:43 +0700

    Added android project

---
+++ app/src/main/java/inc/notchg/workoutapp/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package inc.notchg.workoutapp.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
