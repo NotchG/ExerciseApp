9e56c067d46fcb16d555109c0fe823d64b338777
NotchG <vidoghibran7@gmail.com>
2020-06-17 22:11:18 +0700

    Deleted Login Screen and Added Splash Screen

--- app/src/main/java/inc/notchg/workoutapp/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package inc.notchg.workoutapp.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
