b99df1f0b93ab9c2e312d41b1045921f24613711
NotchG <vidoghibran7@gmail.com>
2020-06-20 02:47:42 +0700

    Added bottom navigation activity

--- app/src/main/java/inc/notchg/workoutapp/SplashScreen.kt
+++ app/src/main/java/inc/notchg/workoutapp/SplashScreen.kt
@@ -1,5 +1,6 @@
 package inc.notchg.workoutapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,18 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(5000)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
     }
 }
---
+++ app/src/main/java/inc/notchg/workoutapp/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package inc.notchg.workoutapp.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
